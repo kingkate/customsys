1c7ca1a9001c5607ecfacd9115703386b0aeacc2
kingkate <kingkate2009@163.com>
2017-01-20 15:27:19 +0800

    custom sys

---
+++ Customsys-login/src/main/java/org/Customsys/login/App.java
@@ -0,0 +1,13 @@
+package org.Customsys.login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Customsys-web/src/main/java/com/kingkate/web/util/PropertyConfigUtil.java
@@ -0,0 +1,30 @@
+package com.kingkate.web.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+public class PropertyConfigUtil extends PropertyPlaceholderConfigurer {
+
+	private static Map<String,Object> propertiesMap;
+	@Override
+	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
+			throws BeansException {
+		super.processProperties(beanFactoryToProcess, props);
+		propertiesMap = new HashMap<>();
+		for(Object key : props.keySet()) {
+			String keyStr = key.toString();
+			String value = props.getProperty(keyStr);
+			propertiesMap.put(keyStr, value);
+		}
+	}
+	
+	public static Object getConfigProperty(String key){
+		return propertiesMap.get(key);
+	}
+
+}
